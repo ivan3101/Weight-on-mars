1a8f6451f76c1cdab0f4b42e153ec069d1afef38
Ivan De Menezes <ivandmenezes@gmail.com>
2021-10-18 00:06:16 +0200

    feat: read user's weight as an input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::io;
+
 const GRAVITY_ON_EARTH: f32 = 9.81;
 const GRAVITY_ON_MARS: f32 = 3.72076;
 
 fn main() {
-    let your_weight: f32 = 90.0;
+    let your_weight = read_user_weight();
     let your_weight_on_mars = calculate_weight_on_mars(your_weight);
 
     println!("Your weight on Mars is: {} kg", your_weight_on_mars)
@@ -10,4 +12,15 @@ fn main() {
 
 fn calculate_weight_on_mars(weight_on_earth: f32) -> f32 {
     (weight_on_earth / GRAVITY_ON_EARTH) * GRAVITY_ON_MARS
+}
+
+fn read_user_weight() -> f32 {
+    let mut input = String::new();
+
+    println!("Please enter your weight in kg:");
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+
+    weight
 }
